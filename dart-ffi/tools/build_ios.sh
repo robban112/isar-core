f68ee982bf7872c51e289e588a885c4da4c83304
Robert Lorentz <robert@surfboard.se>
2022-04-06 14:07:55 +0200

    updated mdbx to 11.0.6

--- dart-ffi/tools/build_ios.sh
+++ dart-ffi/tools/build_ios.sh
@@ -6,4 +6,4 @@ cargo install cargo-lipo
 
 cargo lipo --targets aarch64-apple-ios x86_64-apple-ios --release
 
-mv "target/universal/release/libisar.a" "target/universal/release/libisar_ios.a"
+mv "target/universal/release/libisar.a" "libisar_ios.a"
